2f15697301ddac9c0838d1c493189cfe35699b7b
Tim Garlick <timgarlick11@gmail.com>
2015-12-14 00:39:15 -0700

    deleted a lot of extra code made it more maintable checkbox still not working

--- js/mathGameController.js
+++ js/mathGameController.js
@@ -5,59 +5,49 @@ app.controller("mathGameController", function($scope, mathGameService) {
 	mathGameService.tester();
 
 	var topNumbers = [2,3,5,7,9,2,4,6,8,3];
-	var bottomNumbers = [5,4,6,8,2,3,5,7,9,7];
-
-	$scope.top = [];
-	$scope.bottom = [];
-	$scope.answerSheet = {};
+	
+	$scope.questions = [];
 	$scope.userAnswers = {};
-	$scope.wrongAnswers = [];
+	$scope.answerCheck = [];
+	
 
 	$scope.numberGenerator = function() {
-		$scope.top = [];// empty out the array
-		$scope.bottom = [];//empty out the array
-		$scope.answerSheet = {};//empty out the object
-		$scope.userAnswers = {};// empty out the object
-		$scope.wrongAnswers = [];// empty out object
+		$scope.questions = [];
+		$scope.userAnswers = {};
+		$scope.answerCheck = [];
 		$scope.shower = true;
-
-		var random1 = Math.floor((Math.random() * 3) + 4);
+		$scope.disable = false;
+		var random1 = Math.floor((Math.random() * 3));
 		var random2 = Math.floor((Math.random() * 7) + random1);
 		var random = Math.floor((Math.random() * random2) + random1);
 
 		for (var i = 0; i < topNumbers.length; i++) {
-			var numerator = Math.floor((Math.random() * topNumbers[i]) + random);
-			$scope.top.push(numerator);
-			console.log($scope.top);
-		};
-		for (var i = 0; i < bottomNumbers.length; i++) {
-			var denominator = Math.floor((Math.random() * bottomNumbers[i]) + random);
-			$scope.bottom.push(denominator);
-			console.log($scope.bottom);
-		};
+			var numerator = Math.floor((Math.random() * topNumbers[i]) + random + 2); 
+			var denominator= Math.ceil((Math.random() * topNumbers[i] + 2) + random2);
+			// var answers = numerator + denominator;
 
-		for (var i=0; i < $scope.top.length; i++) {
-			var answerSheet = $scope.top[i] + $scope.bottom[i];
-			$scope.answerSheet[i] = answerSheet;
-			console.log($scope.answerSheet);
+			$scope.questions.push({
+				top: numerator,
+				bottom: denominator
+				// answer: answers,			
+			});
 		}
+
+		console.log($scope.questions)
+
 	};
+	
 	$scope.stopTime = function() {
-		console.log($scope.userAnswers);
-		var tester = angular.equals($scope.answerSheet, $scope.userAnswers);
-		console.log(tester);
-		console.log($scope.answerSheet);
-		console.log($scope.userAnswers);
 		$scope.shower = false;
-		
-		angular.forEach($scope.answerSheet, function(v, i) {
-			console.log(v,i)
-			if ($scope.userAnswers[i] != v) {
-				$scope.wrongAnswers.push(false);
+		$scope.disable = true;
+		angular.forEach($scope.userAnswers, function(v, i) {
+			console.log($scope.questions[i].answer)
+			if ($scope.userAnswers[i] != $scope.questions[i].answer) {
+				$scope.answerCheck.push(false);
 			} else {
-				$scope.wrongAnswers.push(true);
+				$scope.answerCheck.push(true);
 			}
-			console.log($scope.wrongAnswers);
+			console.log($scope.answerCheck);
 		});
 	}
 });
