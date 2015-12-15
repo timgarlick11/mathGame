28acc222c85ee2c30d79e74b6e5c14aea3e98ec6
Tim Garlick <timgarlick11@gmail.com>
2015-12-15 00:41:14 -0700

    added a timer changed some styling added bootstrap

--- js/mathGameController.js
+++ js/mathGameController.js
@@ -1,22 +1,28 @@
 var app = angular.module("mathGame");
 
-app.controller("mathGameController", function($scope, mathGameService) {
+app.controller("mathGameController", function($scope, $interval, mathGameService) {
 
 	mathGameService.tester();
 
 	var topNumbers = [2,3,5,7,9,2,4,6,8,3];
 	
-	$scope.questions = [];
+	$scope.questions = ["","","","","","","","","",""];
 	$scope.userAnswers = {};
-	
+	$scope.answerCheck = [];
+	$scope.buttonDisable = true;
 	
 
 	$scope.numberGenerator = function() {
 		$scope.questions = [];
 		$scope.userAnswers = {};
 		$scope.answerCheck = [];
-		$scope.shower = true;
-		$scope.disable = false;
+		$scope.buttonDisable = true;
+		$interval.cancel(stop);
+		stop  = undefined;
+		$scope.m = "00";
+		$scope.s = "00";
+		count = 0;
+
 		var random1 = Math.floor((Math.random() * 3));
 		var random2 = Math.floor((Math.random() * 7) + random1);
 		var random = Math.floor((Math.random() * random2) + random1);
@@ -32,15 +38,38 @@ app.controller("mathGameController", function($scope, mathGameService) {
 				answer: answers			
 			});
 		}
-
-		console.log($scope.questions)
-
 	};
+
+	var stopWatch = function(seconds)
+	{
+		$scope.m = Math.floor(seconds / 60) % 60;
+		$scope.s = seconds % 60;
+		if ($scope.m  < 10)  $scope.m  = "0" +  $scope.m;
+		if ($scope.s < 10) $scope.s = "0" + $scope.s;
+		//return  $scope.m + ":" + $scope.s;
+	}
+	var count = 0;
+	var stop; // you have to assign a the interval function to a variable so you can pass that variable in to the cancel function to stop the correc timer.
+	$scope.timer = function() {
+		stop = $interval(function ()
+		{
+			count++;
+			stopWatch(count);
+
+		}, 1000)
+	}
+
+	$scope.counter = function() {
+		if (Object.keys($scope.userAnswers).length > 9) {
+			$scope.buttonDisable = false;
+		}
+		console.log(Object.keys($scope.userAnswers).length);
+	}
 	
 	$scope.stopTime = function() {
+		$interval.cancel(stop);
 		$scope.shower = false;
 		$scope.disable = true;
-		$scope.answerCheck = [];
 		angular.forEach($scope.userAnswers, function(v, i) {
 			console.log($scope.questions)
 			if ($scope.userAnswers[i] != $scope.questions[i].answer) {
